e7eb43b6b4eb0cc462f782e76b5b998b79ea8e60
minhnguyen6913 <minhnguyen6913@gmail.com>
2024-06-14 09:56:03 +0700

    init

--- bin/php74/doc2pdf.sh
+++ bin/php74/doc2pdf.sh
@@ -1,1 +1,3 @@
-export HOME=/tmp && soffice --headless --convert-to pdf --outdir $1 $2
+# export HOME=/tmp && soffice --headless --convert-to pdf --outdir $1 $2
+export HOME=/tmp && soffice --headless --convert-to pdf --outdir $1  --convert-options "EmbedFonts=true" --infilter="Microsoft Word 2007-2013 XML" $2
+
